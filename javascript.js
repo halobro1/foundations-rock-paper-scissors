9f821545939692ddba0e3b3b0db953f33acbdf24
songying <gsy1911a1@gmail.com>
2022-06-22 17:49:32 +0800

    git commit

---
+++ javascript.js
@@ -0,0 +1,23 @@
+// Create array containing all choices(rock,paper,scissors)
+// All possible scenarios:
+    //rock > scissors
+    //scissors > paper
+    //paper > rock
+    //computer choice and player choice is the same, return null
+// Match ends after 5 rounds 
+// Create function called computerPlay that will randomly return either 'rock', 'paper'
+// or 'scissors'
+// Create function taking 2 parameters - playerSelection and computerSelection
+
+function computerPlay('rock', 'paper', 'scissors')
+
+
+function playRound(playerSelection, computerSelection) {
+
+
+    for (let i = 0; i < 5; i++)
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection))
