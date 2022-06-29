ecb3606a6a4beee73679248cf2432ace80e7d447
songying <gsy1911a1@gmail.com>
2022-06-29 22:39:31 +0800

    git commit

--- javascript.js
+++ javascript.js
@@ -2,6 +2,7 @@ const selectionButtons = document.querySelectorAll('[data-selection]')
 const finalColumn = document.querySelector('[data-final-column]')
 const computerScoreSpan = document.querySelector('[data-computer-score]')
 const yourScoreSpan = document.querySelector('[data-your-score]')
+const gameWinner = document.querySelector('[data-game-winner]')
 const SELECTIONS = [
     {
         name:'rock',
@@ -24,7 +25,7 @@ selectionButtons.forEach(selectionButton => {
     selectionButton.addEventListener('click', e => {
         const selectionName = selectionButton.dataset.selection
         const selection = SELECTIONS.find(selection => selection.name === selectionName)
-        makeSelection(selectionName)
+        makeSelection(selection)
     })
 })
 
@@ -47,7 +48,7 @@ function incrementScore(scoreSpan){
 
 function addSelectionResult(selection, winner) {
     const div = document.createElement('div')
-    div.innerText(selection.emoji)
+    div.innerText = selection.emoji
     div.classList.add('result-selection')
     if (winner) div.classList.add('winner')
     finalColumn.after(div)
@@ -61,3 +62,14 @@ function randomSelection() {
      const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
      return SELECTIONS[randomIndex]
 }
+
+function logWins() {
+    let playerWins = winners.filter((item) => item == "Player").length;
+    let computerWins = winners.filter((item) => item == "Computer").length;
+    let ties = winners.filter((item) => item == "Tie").length;
+    console.log("Results:");
+    console.log("Player Wins:", playerWins);
+    console.log("Computer Wins:", computerWins);
+    console.log("Ties:", ties);
+  }
+
